05ffc2d6497b7dd0bb8d8ede7c9f12b21d198b05
CODIPHENIX <bwangoastrid@gmail.com>
2024-12-02 00:07:07 +0100

    update  enemy

--- src/enemy/EnemyBase.java
+++ src/enemy/EnemyBase.java
@@ -12,20 +12,5 @@ public class EnemyBase extends Unite {
         return null;
     }
 
-    @Override
-    public int degatCalcul( Unite attaquant) {
-        int attaque = attaquant.getAttaque();
-        int defense = this.getDefence();
-        double multi;
-        TypeUniteJ target = attaquant.getTypeUniteJ();
-        switch (target){
-            case SOLDAT -> multi = 1.5;
-            case ARCHER -> multi = 2.0;
-            case CAVALIER -> multi =3.0;
-            default -> multi=1;
-        }
-       double degatBase = ((double) attaque /( 1 + ((double) defense /10))  * multi);
-        return Math.max((int)Math.round(degatBase),0);
-    }
 
 }
