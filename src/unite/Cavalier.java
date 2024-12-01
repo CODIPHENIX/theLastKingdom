5d0221f870a78cb1d0d503808875e57f9cc5b150
CODIPHENIX <bwangoastrid@gmail.com>
2024-12-02 00:07:43 +0100

    update  unite

--- src/unite/Cavalier.java
+++ src/unite/Cavalier.java
@@ -12,19 +12,5 @@ public class Cavalier extends Unite{
     public String toString() {
         return null;
     }
-    @Override
-    public int degatCalcul( Unite attaquant) {
-        int attaque = attaquant.getAttaque();
-        int defense = this.getDefence();
-        double multi;
-        TypeEnemy target = attaquant.getTypeEnemy();
-        switch (target){
-            case ENEMYBASE -> multi = 1.5;
-            case ENEMYINTERMEDIER -> multi = 2.0;
-            case BOSS -> multi =3.0;
-            default -> multi=1;
-        }
-        double degatBase = ((double) attaque /( 1 + ((double) defense /10))  * multi);
-        return Math.max((int)Math.round(degatBase),0);
-    }
+
 }
--- src/unite/Soldat.java
+++ src/unite/Soldat.java
@@ -17,19 +17,6 @@ public class Soldat extends Unite{
         return null;
     }
 
-    @Override
-    public int degatCalcul( Unite attaquant) {
-        int attaque = attaquant.getAttaque();
-        int defense = this.getDefence();
-        double multi;
-        TypeEnemy target = attaquant.getTypeEnemy();
-        switch (target){
-            case ENEMYBASE -> multi = 1.5;
-            case ENEMYINTERMEDIER -> multi = 2.0;
-            case BOSS -> multi =3.0;
-            default -> multi=1;
-        }
-        double degatBase = ((double) attaque /( 1 + ((double) defense /10))  * multi);
-        return Math.max((int)Math.round(degatBase),0);
-    }
+
+
 }
